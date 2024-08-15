d10b75a188e8d7dd69b0e615aad8189548a0aec3
log-log-log <l.sull1171957@gmail.com>
2024-08-15 17:34:27 -0400

    Fixed script

--- script.ps1
+++ script.ps1
@@ -10,7 +10,8 @@ echo `# <#`
 set -euo pipefail
 
 while true; do
-    date + '%Y-%m-%d %H:%M:%S' > date
+    date +'%Y-%m-%d %H:%M:%S' > date
+    git add date
     git commit -m "Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
     git push -u origin main
     sleep 12h
@@ -23,6 +24,7 @@ exit
 
 while($true){
     $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') > date
+    git add date
     git commit -m "Automated commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
     git push -u origin main
     # Sleep 12 hours (43200 seconds)
